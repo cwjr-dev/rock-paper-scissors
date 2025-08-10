a243f90261f3829126a8ea6ee3f60ed0e550e45c
CulbertW <jrdevpath87@gmail.com>
2025-08-09 19:19:06 -0500

    feat(game-round): Implement logic to play a single round
    - Added playRound function to display a draw or winner after a single round
    - Added humanScore and computerScore global variables to track game scores

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -34,3 +34,37 @@ function getHumanChoice() {
 
 let humanScore = 0;
 let computerScore = 0;
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+/*
+    FUNCTION:
+        playRound
+    DESCRIPTION:
+        Plays a single round of Rock Paper Scissors and displays the round result
+*/
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+
+    const roundSelection = `${humanChoice}-${computerChoice}`;
+    const winCombination = "rock-scissors paper-rock scissors-paper";    
+
+    // determine round winner
+        // draw game
+    if (humanChoice === computerChoice) {
+        console.log(`Draw! Both players chose ${humanChoice}`);
+    }
+    else if (winCombination.includes(roundSelection)) {
+        // human wins
+        console.log(`You win! ${humanSelection} beats ${computerSelection}`);
+        humanScore++;
+    }
+    else {
+        // computer wins
+        console.log(`You Lose! ${computerSelection} beats ${humanSelection}`);
+        computerScore++;
+    }   
+} 
+
+playRound(humanSelection, computerSelection);
